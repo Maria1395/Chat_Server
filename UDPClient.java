381ef86c17e9218b77c5f406d38b327a48c6d0cc
Maria <mariarose1395@gmail.com>
2018-03-23 11:54:23 +0530

    Client updated

--- UDPClient.java
+++ UDPClient.java
@@ -3,3 +3,8 @@ import java.net.*;
  
 class UDPClient
 {
+  
+   public static void main(String args[]) throws Exception
+   {
+      BufferedReader inFromUser =
+         new BufferedReader(new InputStreamReader(System.in));
