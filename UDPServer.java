a6e4f8da59bf852894c627d83290f6d4ef7972e5
Manisha <rlmanus31@gmail.com>
2018-03-23 11:54:37 +0530

    Server Modified

--- UDPServer.java
+++ UDPServer.java
@@ -3,3 +3,7 @@ import java.net.*;
  
 class UDPServer
 {
+ public static void main(String args[]) throws Exception
+      {
+         DatagramSocket serverSocket = new DatagramSocket(9876);
+            byte[] receiveData = new byte[1024];
